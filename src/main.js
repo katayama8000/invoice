a7c127b0bb1f8a68c0ffbbead219946b8cd1d7c7
katayama <tattu.0310@gmail.com>
2022-05-22 16:23:12 +0900

    ⚡️

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import router from './router'
 import store from './store'
 import './assets/tailwind.css'
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore"
 
 //Your web app's Firebase configuration
